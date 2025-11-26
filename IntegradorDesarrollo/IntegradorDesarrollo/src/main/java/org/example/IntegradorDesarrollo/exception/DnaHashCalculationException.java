02ded792643316da18aba1c2ad17a960658eb6f8
Rocío Carvajal <rocarvajal03@gmail.com>
2025-11-26 10:30:02 -0300

    Delete IntegradorDesarrollo directory

--- IntegradorDesarrollo/IntegradorDesarrollo/src/main/java/org/example/IntegradorDesarrollo/exception/DnaHashCalculationException.java
+++
@@ -1,8 +0,0 @@
-package org.example.IntegradorDesarrollo.exception;
-
-public class DnaHashCalculationException extends RuntimeException {
-
-    public DnaHashCalculationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
